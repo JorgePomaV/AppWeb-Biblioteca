2d43eff720c27a2e3a39ecb0ccf894e9bd5fbfbb
JorgePomaV <jpomavergara1418@gmail.com>
2024-10-14 23:05:06 -0300

    se corrigieron errores de AuthController

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,10 +1,12 @@
 <?php
     class AuthController extends BaseController{
-         
+        private $authModel;
+        private $tareaModel;
+        private $estadoModel;
         public function __construct(){
             $this->authModel=$this->model('AuthModel');
             $this->tareaModel=$this->model('TareaModel');
-            $this->estadoModel=  $this->model('EstadoModel');
+            $this->estadoModel= $this->model('EstadoModel');
         }
         
         /* Función para llamar a la vista login con blanqueo de errores*/
@@ -79,16 +81,20 @@
                 $usuario = $_POST['usuario'];
                 $pass = $_POST['password'];
                 $pass2 = $_POST['password2'];
+                //$_FILES['avatar']: Captura la información del archivo subido (en este caso, el avatar del usuario), como el nombre del archivo, el tipo de imagen y el tamaño.
                 $avatar = $_FILES['avatar']['name'];
                 $image_type = $_FILES['avatar']['type'];
                 $image_size = $_FILES['avatar']['size'];
+                //$ubi: Guarda la ruta completa donde se almacenarán las imágenes de avatar, combinando la ruta raíz del servidor y la constante RUTA_AVATAR.
                 $ubi = $_SERVER['DOCUMENT_ROOT'] . RUTA_AVATAR;
 
-                if ($avatar != ''){
-                    if($image_size <= 10000000){
-                        if ($image_type == 'image/jpg' || $image_type == 'image/jpeg' || $image_type == 'image/png') {
+                if ($avatar != ''){//Verifica si se ha cargado un archivo de avatar.
+                    if($image_size <= 10000000){//Verifica si el tamaño del archivo de imagen es menor o igual a 10MB (10,000,000 bytes).
+                        if ($image_type == 'image/jpg' || $image_type == 'image/jpeg' || $image_type == 'image/png')//Comprueba el tipo de imagen.
+                        // Mueve el archivo subido a la ubicación especificada.
                             move_uploaded_file($_FILES['avatar']['tmp_name'], $ubi . $avatar);
                         }else{
+                           // Crea un mensaje de error para el usuario indicando que solo se permiten imágenes de tipo jpg, jpeg o png, y se carga la vista de registro con esos datos.
                             $data = [
                                 'error_tipo' => '<div class="alert alert-danger" role="alert">
                                 El tipo de imagen debe ser jpg, jpeg o png.
@@ -99,6 +105,7 @@
                             $this->view('pages/auth/register',$data);
                         }
                     }else{
+                        //Si el archivo excede el tamaño permitido (10MB), se genera un mensaje de error indicando que el archivo es demasiado grande.
                         $data = [
                             'error_megas' => '<div class="alert alert-danger" role="alert">
                             El tamaño de la imagen es demasiado grande
@@ -108,10 +115,14 @@
                     }
 
                 }else{
+                    //Si no se ha cargado ningún avatar, se asigna una imagen por defecto (img_default.png).
                     $avatar ='img_default.png';
                   
                 }
+
+                //Verifica si las dos contraseñas ingresadas coinciden.
                 if ($pass == $pass2){
+                    //Si las contraseñas coinciden, crea un arreglo $data con los datos del usuario. Luego, llama al modelo authModel para buscar si ya existe un usuario con el mismo email.
                     $data= [
                         'nombre' => $nombre,
                         'apellido' =>$apellido,
@@ -121,6 +132,7 @@
                         'pass2' => $pass2
                     ];
                     $auth = $this->authModel->buscar_por_mail($data);
+
                     if (empty($auth)){
                         if($this->authModel->crear_usuario($data)){
                             $data = [
@@ -146,7 +158,7 @@
                 }
         
         }
-    }
+    
         public function resetPassword(){
             
             $data = [
@@ -156,68 +168,67 @@
             $this->view('pages/auth/forgot-password',$data);
         }
 
-        public function enviar_password()
-    {
-        $email = $_POST['email'];
-        $data = [
-            'email' => $email,
-        ];
-        
-        
-        if (!empty($this->authModel->buscar_por_mail($data))) {
-            $where = "new_pass";
-            include(RUTA_APP . "/mails/mail_pass.php");
-            
-          
-            
-        } else {
+        public function enviar_password(){
+            $email = $_POST['email'];
             $data = [
-                "error_mail"=> "<div class='alert alert-danger' role='alert'>
-                            <p class = 'text-center'>No es un email válido.</p>
-                         </div>",
-                "mail"=>'',
+                'email' => $email,
             ];
-            $this->view('pages/auth/forgot-password', $data);
-        }
-    }
-
-    public function update_pass(){
-        $data = [
-            'mail' => '',
-            'error_mail'=>'',
-            'error_pass'=>'',
-        ];
-        $this->view('pages/auth/updated-password',$data);
-    }
-
-    public function actualizar_password(){
-        $email = $_POST['email'];
-        $passActual =$_POST['pass_actual'];
-        $passNueva = $_POST['pass_nueva'];
-        $passNueva2 = $_POST['pass_nueva2'];
-        if ($passNueva != $passNueva2){
-            $data = [
-                'mail' => '',
-                'error_mail'=>'',
-                'error_pass'=> "<div class='alert alert-danger' role='alert'>
-                <p class = 'text-center'>Las contraseñas no coinciden.</p>
-             </div>",
-            ];
-            $this->view('pages/auth/updated-password',$data);
-        }else{
-            if($this->authModel->change_pass($passNueva, $email)){
+            
+            
+            if (!empty($this->authModel->buscar_por_mail($data))) {
+                $where = "new_pass";
+                include(RUTA_APP . "/mails/mail_pass.php");
+                
+            
+                
+            } else {
                 $data = [
-                    'mail' => '',
-                    'error_mail'=>'',
-                    'error_pass'=> "<div class='alert alert-success' role='alert'>
-                    <p class = 'text-center'>La contraseña fue actualizada</p>
-                 </div>",
+                    "error_mail"=> "<div class='alert alert-danger' role='alert'>
+                                <p class = 'text-center'>No es un email válido.</p>
+                            </div>",
+                    "mail"=>'',
                 ];
-                $this->view('pages/auth/updated-password',$data);
+                $this->view('pages/auth/forgot-password', $data);
             }
         }
 
-    }
+        public function update_pass(){
+            $data = [
+                'mail' => '',
+                'error_mail'=>'',
+                'error_pass'=>'',
+            ];
+            $this->view('pages/auth/updated-password',$data);
+        }
+
+        public function actualizar_password(){
+            $email = $_POST['email'];
+            $passActual =$_POST['pass_actual'];
+            $passNueva = $_POST['pass_nueva'];
+            $passNueva2 = $_POST['pass_nueva2'];
+            if ($passNueva != $passNueva2){
+                $data = [
+                    'mail' => '',
+                    'error_mail'=>'',
+                    'error_pass'=> "<div class='alert alert-danger' role='alert'>
+                    <p class = 'text-center'>Las contraseñas no coinciden.</p>
+                </div>",
+                ];
+                $this->view('pages/auth/updated-password',$data);
+            }else{
+                if($this->authModel->change_pass($passNueva, $email)){
+                    $data = [
+                        'mail' => '',
+                        'error_mail'=>'',
+                        'error_pass'=> "<div class='alert alert-success' role='alert'>
+                        <p class = 'text-center'>La contraseña fue actualizada</p>
+                    </div>",
+                    ];
+                    $this->view('pages/auth/updated-password',$data);
+                }
+            }
+
+        }
 
         /* Método para cerrar la sesión */
 
@@ -228,8 +239,6 @@
             
             $this->view('pages/index');
         }
-    
-   
-   }
+    }
    
 ?>
--- app/views/pages/auth/register.php
+++ app/views/pages/auth/register.php
@@ -4,23 +4,26 @@
 <!--//$_POST['...']: Captura los datos enviados a través del formulario (como nombre, apellido, email, usuario, contraseñas, etc.).-->
     <form class="form" action="<?php echo RUTA_URL; ?>/AuthController/registrarUsuario/" method="POST" enctype="multipart/form-data">
        <h2 class="h2-subtitulo">Crear una cuenta</h2>
-       <div class="mb-3 div-flex">
-          <label for="EntradaNombre" class="form-label">Nombre</label>
-          <input name="nombre" type="text" class="form-control" id="EntradaNombre">
+       <div class="div-container">
+            <div class="mb-3 div-flex div-1">
+               <label for="EntradaNombre" class="form-label">Nombre</label>
+               <input name="nombre" type="text" class="form-control" id="EntradaNombre">
+            </div>
+            <div class="mb-3 div-flex div-2">
+               <label for="EntradaApellido" class="form-label">Apellido</label>
+               <input name="apellido" type="text" class="form-control" id="EntradaApellido">
+            </div>
        </div>
-       <div class="mb-3 div-flex">
-          <label for="EntradaApellido" class="form-label">Apellido</label>
-          <input name="apellido" type="text" class="form-control" id="EntradaApellido">
-       </div>
-       <div class="form-group">
-            <label for="formFile" class="form-label ">Avatar</label>
-            <input name="avatar" class="form-control" type="file" id="formFile">
-      </div>
        <div class="mb-3">
           <label for="EntradaDNI" class="form-label">DNI</label>
           <input name="dni" type="number" class="form-control" id="EntradaDNI" aria-describedby="ayudaDNI">
           <div id="ayudaDNI" class="form_text">Ingrese su DNI sin puntos.</div>
        </div>
+       <div class="mb-3">
+          <label for="formFile" class="form-label ">Avatar</label>
+          <input name="avatar" class="form-control" type="file" id="formFile">
+          <div id="ayudaTamañoArchivo" class="form_text">Tamaño del archivo de imagen menor o igual a 10MB.</div>
+         </div>
        <div class="mb-3">
           <label for="EntradaNumero" class="form-label">Numero de Celular</label>
           <input name="numero" type="number" class="form-control" id="EntradaNumero">
@@ -34,15 +37,20 @@
           <label for="EntradaUsuario" class="form-label">Nombre de Usuario</label>
           <input name="usuario" type="text" class="form-control" id="EntradaUsuario">
        </div>
-       <div class="mb-3 div-flex">
-          <label for="EntradaContraseña" class="form-label">Contraseña</label>
-          <input name="password" type="password" class="form-control" id="EntradaContraseña" aria-describedby="ayudaContraseña">
-          <div id="ayudaContraseña" class="form_text">Ingrese su contraseña</div>
+
+       <div class="div-container">
+            <div class="mb-3 div-flex div-1">
+               <label for="EntradaContraseña" class="form-label">Contraseña</label>
+               <input name="password" type="password" class="form-control" id="EntradaContraseña" aria-describedby="ayudaContraseña">
+               <div id="ayudaContraseña" class="form_text">Ingrese su contraseña.</div>
+            </div>
+            <div class="mb-3 div-flex div-2">
+               <label for="EntradaContraseña2" class="form-label">Contraseña</label>
+               <input name="password2" type="password" class="form-control" id="EntradaContraseña2" aria-describedby="ayudaContraseñaRepetir">
+               <div id="ayudaContraseña2" class="form_text">Repita la contraseña.</div>
+            </div>
        </div>
-       <div class="mb-3 div-flex">
-          <input name="password2" type="password" class="form-control" id="EntradaContraseñaRepetir" aria-describedby="ayudaContraseñaRepetir">
-          <div id="ayudaContraseñaRepetir" class="form_text">Repita la contraseña</div>
-       </div>
+       
        <button type="submit" class="btn btn-primary">Crear cuenta</button>
     </form>
  </div>
